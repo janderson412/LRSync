4a5adbb2ae26d4e7973dda823aad42fa4dc44d30
janderson412 <john@jeanderson.net>
2020-08-16 14:10:30 -0400

    Use argparse for LRSync.py.  Save configuation.

--- LRSync.py
+++ LRSync.py
@@ -9,65 +9,141 @@ import os
 import glob
 import datetime
 import zipfile
+import re
+import json
+from argparse import ArgumentParser
 
-catalogpath = sys.argv[1]
-savedir = sys.argv[2]
-showonly = False
-if ((len(sys.argv) > 3) and (sys.argv[3] == "-showonly")):
-    showonly = True
+verbose_output = False
 
-#print("Catalog: " + catalogpath + " Backup folder: " + savedir + " -showonly=" + str(showonly))
 
-# get modified time of local catalog
-catalog_exists = os.path.isfile(catalogpath)
-#print("catalog_exists = " + catalog_exists)
-if catalog_exists:
-    catalog_time = os.path.getmtime(catalogpath)
-    print("Catalog: " + catalogpath + " " + datetime.datetime.fromtimestamp(catalog_time).strftime("%c"))
-else:
-    catalog_time = datetime.MINYEAR
-    print("Catalog: " + catalogpath + " does not exist.")
+def message(message):
+    if verbose_output:
+        print(message)
 
-# find newest backups
-search = savedir + "/**/*.zip"
-newest = datetime.MINYEAR
-newest_file = ""
-print("Searching: " + search)
-for zip in glob.iglob(search, recursive=True):
-    filetime = os.path.getmtime(zip)
-    if filetime > newest:
-        newest = filetime
-        newest_file = zip
-#    print(zipfile + "Modified: " + filetime.strftime("%c"))
 
-if newest_file == "":
-    print("No .zip backup files found, exiting...")
-    exit(0)
+def update_catalog(catalogpath, backupfolder, showonly=False):
+    # get modified time of local catalog
+    catalog_time = datetime.datetime.fromtimestamp(os.path.getmtime(catalogpath))
 
-if catalog_exists and catalog_time >= newest:
-    print("Local catalog is up to date, exiting...")
-    exit(0)
+    # find newest backups
+    search = backupfolder + "/**/*.zip"
+    """def get_immediate_subdirectories(a_dir):
+    return [name for name in os.listdir(a_dir)
+            if os.path.isdir(os.path.join(a_dir, name))]"""
 
-print("Restoring backup: " + newest_file + " " + datetime.datetime.fromtimestamp(filetime).strftime("%c"))
+    catalog_filename = os.path.split(catalogpath)[1]
+    backup_subfolders = [d for d in os.listdir(backupfolder) if os.path.isdir(os.path.join(backupfolder, d))]
+    newest = None
+    newest_backup_file = None
+    for d in backup_subfolders:
+        groups = re.match(r'(\d{4})-(\d{2})-(\d{2}) (\d{2})(\d{2})', d)
+        if groups:
+            backup_path = os.path.join(backupfolder, d, catalog_filename + '.zip')
+            if not os.path.exists(backup_path):
+                continue
+            year = int(groups[1])
+            month = int(groups[2])
+            day = int(groups[3])
+            hour = int(groups[4])
+            minute = int(groups[5])
+            ts = datetime.datetime(year, month, day, hour, minute)
+            if newest is None or ts > newest:
+                newest = ts
+                newest_backup_file = backup_path
 
-zipfile_to_restore = zipfile.ZipFile(newest_file)
-found = False
-catalog_file = os.path.basename(catalogpath)
-for saved_catalog in zipfile_to_restore.namelist():
-    if (saved_catalog == catalog_file):
-        found = True
-        break
+    if newest_backup_file is None:
+        message(f'No .zip backup files found for {catalogpath}.')
+        return True
 
-if not found:
-    print("Catalog file not found.")
-    exit(1)
+    if catalog_time >= newest:
+        message(f'Local catalog "{catalogpath}" is newer than "{newest_backup_file}", no update.')
+        return True
 
-# copy backed up catalog, overwriting old...
-if (showonly):
-    exit(0)
+    message(f'Restoring backup:{newest_backup_file} ({newest})')
 
-print("Extracting...")
-#restore_path = "/Users/john"
-restore_path = os.path.dirname(catalogpath)
-print("Restoring: " + catalog_file + " to: " + restore_path)
-zipfile_to_restore.extract(catalog_file, restore_path)
+    zipfile_to_restore = zipfile.ZipFile(newest_backup_file)
+    found = False
+    catalog_file = os.path.basename(catalogpath)
+    for saved_catalog in zipfile_to_restore.namelist():
+        if (saved_catalog == catalog_file):
+            found = True
+            break
+
+    if not found:
+        message('Catalog file not found.')
+        exit(1)
+
+    message("Extracting...")
+    restore_path = os.path.dirname(catalogpath)
+    message(f'Restoring: "{catalog_file}" to "{restore_path}"')
+    if showonly:
+        message('--showonly: skipping actual copy')
+        return True
+    zipfile_to_restore.extract(catalog_file, restore_path)
+    return True
+
+
+def get_folders(system_name, folders):
+    CATALOG = 'catalog_folder'
+    BACKUP = 'backup_folder'
+    catalogfolder = folders[0]
+    backupfolder = folders[1]
+    json_filename = 'LRSync.json'
+    if os.path.exists(json_filename):
+        with open(json_filename, 'r') as f:
+            info = json.load(f)
+    else:
+        info = dict()
+
+    if system_name in info:
+        if catalogfolder is None:
+            catalogfolder = info[system_name][CATALOG]
+        else:
+            message(f'Saving catalogfolder="{catalogfolder}" for "{system_name}" to configuration file.')
+            info[system_name][CATALOG] = catalogfolder
+        if backupfolder is None:
+            backupfolder = info[system_name][BACKUP]
+        else:
+            message(f'Saving backupfolder="{backupfolder}" for "{system_name}" to configuration file.')
+            info[system_name][BACKUP] = backupfolder
+    else:
+        if catalogfolder is None or backupfolder is None:
+            return False
+        message(f'Creating folder information for "{system_name}" : catalogfolder="{catalogfolder}" backupfolder="{backupfolder}"')
+        info[system_name] = {CATALOG: catalogfolder,
+                             BACKUP: backupfolder}
+
+    with open(json_filename, 'w') as f:
+        json.dump(info, f, indent=4)
+
+    folders[0] = catalogfolder
+    folders[1] = backupfolder
+    return True
+
+
+def update_catalogs(catalogfolder, backupfolder, showonly=False):
+    # get catalog filenames
+    catalog_filenames = glob.glob(catalogfolder + '/*.lrcat')
+    for catalog in catalog_filenames:
+        update_catalog(catalog, backupfolder, showonly)
+
+if __name__ == '__main__':
+    system_name = os.uname()[1]
+    parser = ArgumentParser()
+    parser.add_argument('--catalogfolder', help='Folder where local Lightroom catalogs maintained', required=False)
+    parser.add_argument('--backupfolder', help='Folder containing catalog backups', required=False)
+    parser.add_argument('--showonly', help='Take no action, only show operations that would be performed',
+                        action='store_true')
+    parser.add_argument('--verbose', help='Verbose output', action='store_true', required=False, default=False)
+    args = parser.parse_args()
+    verbose_output = args.verbose
+    folders = [args.catalogfolder, args.backupfolder]
+    if not get_folders(system_name, folders):
+        print(f'--catalogfoler and/or --backupfolder not given and no existing info for "{system_name}" in configuration')
+        sys.exit(1)
+
+    result = update_catalogs(folders[0], folders[1], args.showonly)
+    if result:
+        sys.exit(0)
+    else:
+        sys.exit(1)
